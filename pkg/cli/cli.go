9c315f4709cf7f9db4f576b0a415a27ec902e739
Ioannis Varouchakis <johnvarouchakis@gmail.com>
2025-06-11 22:18:58 +0200

    improve flag setup

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -9,36 +9,25 @@ import (
 )
 
 func Start() {
-	help := flag.Bool("help", false, "Print help message")
-	tool := flag.NewFlagSet("tool", flag.ExitOnError)
-	base64 := tool.String("base64", "", "base64 (encode/decode)")
-	uuid := tool.Bool("uuid", false, "get a uuid")
-	json := tool.String("json", "", "json (beautify/minify)")
+	base64 := flag.String("base64", "", "base64 (encode/decode)")
+	uuid := flag.Bool("uuid", false, "get a uuid")
+	json := flag.String("json", "", "json (beautify/minify)")
 	var result string
 
 	flag.Parse()
 
-	if len(os.Args) == 1 || *help {
-		fmt.Printf("rtools tool [-base64|-json|-uuid]\n\nUsage of tool:\n  -base64 string\n      base64 (encode/decode)\n  -json string\n      json (beautify/minify)\n  -uuid\n      get a uuid\n")
-	} else if os.Args[1] == "tool" {
-		tool.Parse(os.Args[2:])
-
-		if *base64 == "encode" {
-			result = tools.Base64Encode(os.Args[4])
-		} else if *base64 == "decode" {
-			result = tools.Base64Decode(os.Args[4])
-		} else if *uuid == true {
-			result = tools.NewUuid()
-		} else if *json == "beautify" {
-			result = tools.JSONBeautify(os.Args[4])
-		} else if *json == "minify" {
-			result = tools.JSONMinify(os.Args[4])
-		} else {
-			fmt.Printf("Unkown command: %v\n", os.Args[2:])
-		}
+	if *base64 == "encode" && len(os.Args) > 3 {
+		result = tools.Base64Encode(os.Args[3])
+	} else if *base64 == "decode" && len(os.Args) > 3 {
+		result = tools.Base64Decode(os.Args[3])
+	} else if *uuid == true {
+		result = tools.NewUuid()
+	} else if *json == "beautify" && len(os.Args) > 3 {
+		result = tools.JSONBeautify(os.Args[3])
+	} else if *json == "minify" && len(os.Args) > 3 {
+		result = tools.JSONMinify(os.Args[3])
 	} else {
-		fmt.Printf("Unkown tool: %s\n", os.Args[1])
-		os.Exit(1)
+		flag.PrintDefaults()
 	}
 
 	fmt.Printf("%s\n", result)
